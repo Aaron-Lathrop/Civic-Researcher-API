c4efb603f48d5c38af7b99286735b853e4322b06
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-07-11 08:10:24 -0600

    added code for float-grid.css, added pseudocode for index.js, added boilerplate and starter text to index.html, added comment to main.css

---
+++ index.js
@@ -0,0 +1,61 @@
+// This is where I'll write tons of awesome javascript
+
+//Google Civic API code
+function getDataGoogleCivicAPI {
+    console.log(`getDataGoogleCivicAPI ran`);
+}
+
+function renderGoogleCivic {
+    console.log(`renderGoogleCivic ran`);
+}
+
+function displayGoogleCivic {
+    console.log(`displayGoogleCivic ran`);
+}
+
+function watchCivicSubmit {
+    console.log(`watchCivicSubmit ran`);
+}
+
+
+//Google Search API code
+function getDataGoogleSearchAPI {
+    console.log(`getDataGoogleSearchAPI ran`);
+}
+
+function renderGoogleSearch {
+    console.log(`renderGoogleSearch ran`);
+}
+
+function displayGoogleSearch {
+    console.log(`displayGoogleSearch ran`);
+}
+
+function watchSearchSubmit {
+    console.log(`watchCivicSubmit ran`);
+}
+
+
+//YouTube API code
+function getDataYouTubeAPI {
+    console.log(`getDataYouTubeAPI ran`);
+}
+
+function renderYouTube {
+    console.log(`renderYouTube ran`);
+}
+
+function displayYouTube {
+    console.log(`displayGoogleCivic ran`);
+}
+
+function watchSearchSubmit {
+    console.log(`watchCivicSubmit ran`);
+}
+
+
+//General functionality
+
+function watchAddressSubmit {
+    console.log(`watchAddressSubmit ran`);
+}
