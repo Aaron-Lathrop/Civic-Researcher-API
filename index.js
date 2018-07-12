37138e95362102e0843c69e61fa19c1f4ff4b3ef
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-07-11 19:53:05 -0600

    added basic code for YouTube API in index.js

--- index.js
+++ index.js
@@ -9,53 +9,63 @@ function renderGoogleCivic {
     console.log(`renderGoogleCivic ran`);
 }
 
-function displayGoogleCivic {
+function displayGoogleCivic() {
     console.log(`displayGoogleCivic ran`);
 }
 
-function watchCivicSubmit {
+function watchCivicSubmit() {
     console.log(`watchCivicSubmit ran`);
 }
 
 
 //Google Search API code
-function getDataGoogleSearchAPI {
+function getDataGoogleSearchAPI() {
     console.log(`getDataGoogleSearchAPI ran`);
 }
 
-function renderGoogleSearch {
+function renderGoogleSearch() {
     console.log(`renderGoogleSearch ran`);
 }
 
-function displayGoogleSearch {
+function displayGoogleSearch() {
     console.log(`displayGoogleSearch ran`);
 }
 
-function watchSearchSubmit {
+function watchSearchSubmit() {
     console.log(`watchCivicSubmit ran`);
 }
 
 
 //YouTube API code
-function getDataYouTubeAPI {
-    console.log(`getDataYouTubeAPI ran`);
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const APIkey = 'AIzaSyAeXH1TNL5oTEVqbPGq8rrh6DF8WTbkKHE';
+
+
+function getDataYouTubeAPI(searchTerm, callback) {
+    const settings = {
+        part: 'snippet',
+        key: APIkey,
+        q: `${searchTerm}`
+    }
+    $.getJSON(YOUTUBE_SEARCH_URL, settings, callback);
 }
 
-function renderYouTube {
+function renderYouTube() {
     console.log(`renderYouTube ran`);
 }
 
-function displayYouTube {
-    console.log(`displayGoogleCivic ran`);
+function displayYouTube() {
+    const results = data.items.map((item, index) => renderYouTube(item));
+    $('#js-YouTube-results').html( results );
 }
 
-function watchSearchSubmit {
+function watchYouTubeSubmit() {
     console.log(`watchCivicSubmit ran`);
 }
 
 
 //General functionality
 
-function watchAddressSubmit {
+function watchAddressSubmit() {
     console.log(`watchAddressSubmit ran`);
 }
